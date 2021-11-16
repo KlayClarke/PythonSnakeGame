c6bc428d9accc16a76a6f295afcd8fdc1c9d9c8f
Klay Clarke <klayaclarke@gmail.com>
2021-11-16 14:40:22 -0500

    initial commit; create screen for snake game

---
+++ main.py
@@ -0,0 +1,7 @@
+from turtle import Turtle, Screen
+
+my_screen = Screen()
+my_screen.setup(width=600, height=600)
+my_screen.bgcolor('black')
+my_screen.title('Python Snake Game')
+my_screen.exitonclick()
