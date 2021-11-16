48bfc6a2b060f82f8f65d0cff8e6289910a13f3d
Klay Clarke <klayaclarke@gmail.com>
2021-11-16 14:54:12 -0500

    create snake using for loop

--- main.py
+++ main.py
@@ -1,42 +1,23 @@
 from turtle import Turtle, Screen
 
-snake = Turtle('square')
-snake.color('white')
 
-snake_2 = Turtle('square')
-snake_2.setpos(x=-20, y=0)
-snake_2.color('white')
-
-snake_3 = Turtle('square')
-snake_3.setpos(x=-40, y=0)
-snake_3.color('white')
+snakes = ['snake_1', 'snake_2', 'snake_3']
 
 
+def create_snake():
+    starting_positions = 0
+    for snake in snakes:
+        snake = Turtle('square')
+        snake.setpos(x=starting_positions, y=0)
+        starting_positions -= 20
+        snake.color('white')
 
-
-
-
-
-
-
-
+create_snake()
 
 
 my_screen = Screen()
 my_screen.setup(width=600, height=600)
 my_screen.bgcolor('black')
 my_screen.title('Python Snake Game')
-
-
-
-
-
-
-
-
-
-
-
-
 
 my_screen.exitonclick()
