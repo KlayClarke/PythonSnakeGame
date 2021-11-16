e646f10c6640b8d8e0c9619f6d0cd8d83ee3c224
Klay Clarke <klayaclarke@gmail.com>
2021-11-16 15:20:49 -0500

    snake able to move and turn fluently no matter the length

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
+import time
 from turtle import Turtle, Screen
 
-
-snakes = ['snake_1', 'snake_2', 'snake_3']
-
-
-def create_snake():
-    starting_positions = 0
-    for snake in snakes:
-        snake = Turtle('square')
-        snake.setpos(x=starting_positions, y=0)
-        starting_positions -= 20
-        snake.color('white')
-
-create_snake()
-
-
+squares = ['square_1', 'square_2', 'square_3']
+snake = []
 my_screen = Screen()
 my_screen.setup(width=600, height=600)
 my_screen.bgcolor('black')
 my_screen.title('Python Snake Game')
+my_screen.tracer(0)
+
+starting_positions = 0
+
+for square in squares:
+    square = Turtle('square')
+    square.setpos(x=starting_positions, y=0)
+    starting_positions -= 20
+    square.color('white')
+    square.penup()
+    snake.append(square)
+
+game_is_on = True
+
+while game_is_on:
+    my_screen.update()
+    time.sleep(0.1)
+    for seg_num in range(len(snake) - 1, 0, -1):
+        new_x_cord = snake[seg_num - 1].xcor()
+        new_y_cord = snake[seg_num - 1].ycor()
+        snake[seg_num].goto(x=new_x_cord, y=new_y_cord)
+    snake[0].forward(20)
+
+# while game_is_on:
+#     for square in squares:
+
 
 my_screen.exitonclick()
